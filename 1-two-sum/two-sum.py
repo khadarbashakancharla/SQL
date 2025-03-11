ce1c5dbf130f8bf9548328d1a6ce208282daa6f0
khadarbashakancharla <122791410+khadarbashakancharla@users.noreply.github.com>
2025-03-11 17:22:43 +0530

    Delete 1-two-sum directory

--- 1-two-sum/two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict = {}
-        for index,value in enumerate(nums):
-            diff = target - value
-            if diff in dict:
-                return [dict[diff],index]
-            dict[value] = index    
-        
